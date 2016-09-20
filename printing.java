821889715977401f2013174969001cfc85f0b68e
pinetree822 <pineree822@nate.com>
2016-09-20 17:59:13 +0900

    no message

---
+++ printing.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+
+class  Hello
+{
+	public static void main(String[] args) 
+	{//main() 메소드의 시작 {}=>몸체
+		System.out.println("Hello World!");
+		System.out.println("Goood Day!");
+		System.out.println("bye!");
+	}//main() 메소드의 끝
+}
